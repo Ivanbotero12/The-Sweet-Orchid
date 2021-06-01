6c04f6b19887ee020b656fb75534ea1d7cf12d42
MartinWifstrand <mart48w2@edu.easj.dk>
2021-06-02 00:18:01 +0200

    Opdateret Comments i Java

--- javakonti.js
+++ javakonti.js
@@ -1,6 +1,11 @@
+// ID'et "show-login" findes, og når den klikkes på, tilføjer den "Active" class'en, //
+// som får Popupet frem på skærmen, da den nye lokation følger "Active" //
+
 document.querySelector("#show-login").addEventListener("click",function(){
     document.querySelector(".popup").classList.add("active")
 });
+
+// Her fjerner den det igen //
 
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active")
